4fef369627ec2a2e70eef4bd24e33997ddb7a3c1
Daniel de Jesus <danieldejesus8745@gmail.com>
2023-04-11 21:52:00 -0300

    Implementing chat functions

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const form = document.getElementById('form');
+let id = localStorage.getItem('id') || 1;
+const ulMessages = document.getElementById('ulMessages');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -6,5 +8,34 @@ form.addEventListener('submit', e => {
   const message = Object.create(null);
   message.text = e.target.message.value.trim();
 
-  console.log(message);
+  addMessage(message);
+  form.reset();
+  loadMessages();
 });
+
+function addMessage(message) {
+  message.id = Number(id);
+  id++;
+  let messages = getMessages();
+  messages.push(message);
+  localStorage.setItem('messages', JSON.stringify(messages));
+  localStorage.setItem('id', id);
+}
+
+function getMessages() {
+  return JSON.parse(localStorage.getItem('messages')) || [];
+}
+
+function loadMessages() {
+  const messages = getMessages();
+
+  ulMessages.innerHTML = '';
+
+  messages.forEach(message => {
+    const li = document.createElement('li');
+    li.textContent = message.text;
+    ulMessages.append(li);
+  });
+}
+
+loadMessages();
